a611fdb203f63ba1dda19ba3f18a8db66ae33eeb
Mercurius360 <97831823+Mercurius360@users.noreply.github.com>
2025-12-08 14:03:10 -0600

    Delete build_exe.bat

--- build_exe.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-pyinstaller --onefile --windowed --name "SCS_File_Updater" ^
-    --icon=icon.ico ^
-    --add-data "README.md;." ^
-    main.py
-echo.
-echo Build complete. Executable is in dist\SCS_File_Updater.exe
-pause
